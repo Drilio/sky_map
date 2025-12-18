6a1d6b44bcd811c14e68d17c2ed22e9495efb0a2
Dominique Gagnière <dominique@gagnie.re>
2025-12-18 11:28:37 +0100

    create topbrigh endpoint and add dumb date for testing purpose

--- API/app/skyapi/urls.py
+++ API/app/skyapi/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SkyStateView
+from .views import Date, TopBrightness
 
 urlpatterns = [
-    path('state/', SkyStateView.as_view()),
+    path('date/', Date.as_view()),
+    path('topbrightness/', TopBrightness.as_view())
 ]
